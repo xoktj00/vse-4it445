af34bb281613b874a7eec17be0977e547ee7339f
Oktábec Jiří <jirka.oktabec@gmail.com>
2016-09-27 18:18:31 +0200

    updates from first lesson

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends Component{
+  render(){
+    return (
+
+      <h1>Protoze ...{this.props.title} + {this.props.code2}</h1>
+    );
+  }
+}
+
 class App extends Component {
   render() {
+    const code1 = 'code123';
+    const array1 = [
+      ["1","jedna"],["2","dva"]
+    ];
+
     return (
       <div className="App">
+      {array1.map(([code1,title])=>
+        <Header code={code1} title={title}/>
+      )}
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -15,6 +32,7 @@ class App extends Component {
         </p>
       </div>
     );
+
   }
 }
 
