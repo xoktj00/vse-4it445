df646c7ba05ce111f073bc763339f92bd8a49a8d
Oktábec Jiří <jirka.oktabec@gmail.com>
2016-10-11 18:06:18 +0200

    y

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -3,37 +3,35 @@ import axios from 'axios';
 import { ProductList } from '../components/ProductList/ProductList.js';
 
 export class ProductsPage extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        products: [],
+      };
+    }
+
   componentDidMount(){
     console.log("did mount");
 
     axios('http://dev.backend.xoktj00.vse.handson.pro/api/products').then((response) => {
+      this.setState({
+        products: response.data
+      });
       console.log('response',response.data);
     })
   }
+
+  funkce(){
+    console.log('this',this)
+  }
   render() {
-    const products = [
-      {
-        id: 1,
-        title: 'Škoda Superb',
-        price: '750 000',
-        shortInfo: 'Luxury car produced in the Czech Republic.',
-      },
-      {
-        id: 2,
-        title: 'Ford Focus',
-        price: '600 000',
-        shortInfo: 'Sports car made in USA.',
-      },
-      {
-        id: 3,
-        title: 'Moped',
-        price: '1 000',
-        shortInfo: 'No comment.',
-      },
-    ]
+    const products = this.state.products;
     return (
       <div>
         <div className="jumbotron">
+       <button onClick={this.funkce.bind(this)}>
+            CLickem
+          </button>
           <h1>All Products</h1>
         </div>
         <ProductList products={products}/>
